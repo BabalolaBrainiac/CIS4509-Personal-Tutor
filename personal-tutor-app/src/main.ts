e9244b98226d2fb71a2c62983a8a829f83dd630f
Brainiac <74297323+BabalolaBrainiac@users.noreply.github.com>
2024-04-29 08:13:39 +0100

    configured swagger

--- personal-tutor-app/src/main.ts
+++ personal-tutor-app/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
+
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000, "0.0.0.0");
+
+  const options = new DocumentBuilder()
+      .setTitle('CIS 4509 Personal Tutor Application')
+      .setDescription('CIS 4509 Personal Tutor Application: Group Members Include Amen Victor, Champion OvuaKnporie, Kenechukwu Nkwonta, Olakunle Afolabi, Babalola Opeyemi Daniel')
+      .setVersion('1.0')
+      .addServer('http://localhost:3000/', 'Local environment')
+      .addServer('https://ravishing-art-development.up.railway.app', 'Production')
+      .addTag('CIS 4509 Personal Tutor Application')
+      .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT || 3000, );
 }
 bootstrap();
